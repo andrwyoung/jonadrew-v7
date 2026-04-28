68308590cb3d59649817d0459aa127241d62f589
andrwyoung <yong.andrew11@gmail.com>
2026-04-28 16:59:59 -0700

    add shipping address

--- app/api/stripe/checkout/route.ts
+++ app/api/stripe/checkout/route.ts
@@ -19,7 +19,9 @@ export async function POST(req: NextRequest) {
   try {
     const session = await stripeClient.checkout.sessions.create({
       mode: "payment",
+      allow_promotion_codes: true,
       payment_method_types: ["card"],
+
       line_items: items.map((item) => ({
         quantity: item.quantity,
         price_data: {
@@ -33,6 +35,25 @@ export async function POST(req: NextRequest) {
           },
         },
       })),
+
+      shipping_address_collection: {
+        allowed_countries: ["US", "CA"],
+      },
+
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: { amount: 500, currency: "usd" }, // $5
+            display_name: "Standard Shipping",
+            delivery_estimate: {
+              minimum: { unit: "business_day", value: 5 },
+              maximum: { unit: "business_day", value: 10 },
+            },
+          },
+        },
+      ],
+
       success_url: `${req.nextUrl.origin}/store/success`,
       cancel_url: `${req.nextUrl.origin}/store`,
     });
